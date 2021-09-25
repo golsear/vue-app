e59c7b5489637db6d64658fdd1abe800d725c52e
Sergiy Golub <sergiy.golub@pixabit-interactive.de>
2021-09-25 21:10:17 +0300

    ListView: add name, genre models

--- src/js/components/ListView.vue
+++ src/js/components/ListView.vue
@@ -5,6 +5,17 @@
                 <h1>List view</h1>
             </div>
         </div>
+        <div class="filters">
+            {{ genre }}
+            <select v-model="genre">
+                <option value="">Please select genre</option>
+                <option v-for="option in genreOptions" :value="option.value">
+                    {{ option.text }}
+                </option>
+            </select>
+            <input v-model="name" placeholder="Name" />
+            <button class="btn" @click="searchMovies()">Search</button>
+        </div>
         <template v-for="movie in movies">
             <div class="row">
                 <div class="col-12 col-md-4"><img :src="movie.image" /></div>
@@ -29,7 +40,17 @@
     export default {
         data() {
             return {
-                movies: []
+                movies: [],
+                name: '',
+                genre: '',
+                genreOptions: [
+                    { text: 'ACTION', value: '0' },
+                    { text: 'ADVENTURES', value: '1' },
+                    { text: 'COMEDY', value: '2' },
+                    { text: 'DRAMA', value: '3' },
+                    { text: 'HORROR', value: '4' },
+                    { text: 'WESTERNS', value: '5' }
+                ]
             }
         },
         methods: {
@@ -37,6 +58,32 @@
                 let self = this;
 
                 axios.get('https://cinema-api-test.y-media.io/v1/movies')
+                    .then(response => {
+                        let data = response.data;
+
+                        self.movies = data.data;
+                        this.$store.commit('updateMovies', data.data);
+                    })
+                    .catch(e => {
+                        alert('Something went wrong: ListView : getMovies');
+                    });
+            },
+            searchMovies: function () {
+                let self = this,
+                params = '?';
+                if (self.name) {
+                    params += 'name=' + self.name + '&';
+                }
+
+                if (self.genre) {
+                    params += 'genres=' + self.genre;
+                    console.log('genre', self.genre);
+                }
+
+                console.log(params);
+
+                axios.get('https://cinema-api-test.y-media.io/v1/movies' + params)
+               //axios.get('https://cinema-api-test.y-media.io/v1/movies?genres=comedy')
                     .then(response => {
                         let data = response.data;
 
